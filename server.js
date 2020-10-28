cc9a4977f4acfd6d7b7368583ca8ce346176add8
Methkal <methkal.khalawi@gmail.com>
2020-10-28 14:43:26 +0000

    auto deploy to cloud run using cloud build

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', async (req, res) => {
+  res.send("Built with Nodejs and deployed using Cloud Build and Cloud Run");
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
